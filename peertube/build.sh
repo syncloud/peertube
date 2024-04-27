5af3ae780dd02feb4df5a90b1bd951d18dac6c0c
Boris Rybalkin <ribalkin@gmail.com>
2024-04-27 18:15:32 +0100

    yarn

--- peertube/build.sh
+++ peertube/build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/sh -xe
 
 DIR=$( cd "$( dirname "$0" )" && pwd )
 cd ${DIR}
@@ -7,7 +7,17 @@ VERSION=$1
 BUILD_DIR=${DIR}/../build/snap/webui
 mkdir -p $BUILD_DIR/bin
 
+apt install -y wget
+
 cd ${DIR}/../build
 wget https://github.com/Chocobozzz/PeerTube/releases/download/v$VERSION/peertube-v$VERSION.tar.xz
 tar xf peertube-v$VERSION.tar.xz
-cp -r ${DIR}/bin/* ${BUILD_DIR}/bin
+rm -rf peertube-v$VERSION.tar.xz
+cd peertube-v$VERSION
+yarn install --production --pure-lockfile
+cd ..
+mv peertube-v$VERSION ${BUILD_DIR}/peertube
+cp -r /opt ${BUILD_DIR}
+cp -r /usr ${BUILD_DIR}
+cp -r /bin ${BUILD_DIR}
+cp -r /lib ${BUILD_DIR}
