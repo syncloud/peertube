3b66ed6449db7f52bd5fedcd9cba1cfe9b7303d8
Boris Rybalkin <ribalkin@gmail.com>
2024-05-05 18:14:49 +0100

    fix redis

--- redis/build.sh
+++ redis/build.sh
@@ -3,7 +3,7 @@
 DIR=$( cd "$( dirname "$0" )" && pwd )
 cd ${DIR}
 BUILD_DIR=${DIR}/../build/snap/redis
-mkdir $BUILD_DIR
+mkdir -p $BUILD_DIR
 cp -r /usr ${BUILD_DIR}
 cp -r /lib ${BUILD_DIR}
 cp -r ${DIR}/bin ${BUILD_DIR}/bin
