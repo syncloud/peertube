15478af2643fee018e314ae65fe2442aa8612354
Boris Rybalkin <ribalkin@gmail.com>
2024-05-06 22:41:06 +0100

    node

--- peertube/test.sh
+++ peertube/test.sh
@@ -4,7 +4,7 @@ DIR=$( cd "$( dirname "$0" )" && pwd )
 cd ${DIR}
 
 BUILD_DIR=${DIR}/../build/snap/peertube
-${BUILD_DIR}/bin/node.sh --version
+${BUILD_DIR}/bin/node --version
 
 ldd ${BUILD_DIR}/usr/bin/ffmpeg
 
