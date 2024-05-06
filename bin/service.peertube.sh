913f7c7ab83496192041c75038eb6da3a4e6fccf
Boris Rybalkin <ribalkin@gmail.com>
2024-05-06 09:19:23 +0100

    ffmpeg

--- bin/service.peertube.sh
+++ bin/service.peertube.sh
@@ -1,9 +1,8 @@
 #!/bin/bash -e
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
-#export PATH=$PATH:${DIR}/webui/bin
 export NODE_ENV=production
 export NODE_CONFIG_DIR=/var/snap/peertube/current/config
-
+export PATH=$PATH:${DIR}/peertube/bin
 cd ${DIR}/peertube/app
 exec ${DIR}/peertube/bin/node.sh dist/server
 
