66bbdd0c932902b8f832748f155f787f4828e304
Boris Rybalkin <ribalkin@gmail.com>
2024-05-17 07:33:33 +0100

    disable ssl verify

--- bin/service.peertube.sh
+++ bin/service.peertube.sh
@@ -6,4 +6,5 @@ export PATH=$PATH:${DIR}/peertube/bin
 cd ${DIR}/peertube/app
 /bin/rm -f /var/snap/peertube/current/peertube.socket
 $DIR/bin/wait-for-configure.sh
+export NODE_TLS_REJECT_UNAUTHORIZED=0
 exec node dist/server
